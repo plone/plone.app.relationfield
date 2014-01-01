f3e7af55f07bfd2ac4cde9d8fa45873745fbbdcd
Steve McMahon <steve@dcn.org>
2014-01-01 15:40:34 -0800

    checkpoint

--- plone/app/relationfield/exportimport.py
+++ plone/app/relationfield/exportimport.py
@@ -1,7 +1,9 @@
 from plone.app.relationfield import RelationChoice, RelationList
+from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.supermodel.exportimport import BaseHandler
 from plone.supermodel.utils import noNS, valueToElement, elementToValue
 from zope.interface import Interface
+from zope.schema.interfaces import IContextSourceBinder
 
 import zope.schema
 
@@ -13,7 +15,15 @@ class RelationChoiceHandlerClass(BaseHandler):
     """
 
     filteredAttributes = BaseHandler.filteredAttributes.copy()
-    filteredAttributes.update({'source': 'w', })
+    # we're never going to write a source to XML, and we'll have
+    # a custom reader for portal_types
+    filteredAttributes.update({
+        'source': 'w',
+        'vocabulary': 'w',
+        'vocabularyName': 'w',
+        'values': 'w',
+        'portal_types': 'rw',
+        })
 
     def __init__(self, klass):
         super(RelationChoiceHandlerClass, self).__init__(klass)
@@ -21,30 +31,82 @@ class RelationChoiceHandlerClass(BaseHandler):
         self.fieldAttributes['source'] = \
             zope.schema.Object(__name__='source', title=u"Source", schema=Interface)
 
+        self.fieldAttributes['portal_types'] = \
+            zope.schema.Set(
+                __name__='portal_types',
+                title=u"Portal Types",
+                value_type=zope.schema.TextLine(),
+                )
+
+    def _constructField(self, attributes):
+        # In our override of read, we use the superclass' read before
+        # looking for portal_types. It tries to construct the field,
+        # so we need to make sure it doesn't fail if there's no
+        # source or vocabulary attributes yet.
+        if 'source' not in attributes and \
+           'values' not in attributes and \
+           'vocabulary' not in attributes and \
+           'vocabularyName' not in attributes:
+            attributes['source'] = ObjPathSourceBinder()
+        return self.klass(**attributes)
+
+    def read(self, element):
+        field = super(RelationChoiceHandlerClass, self).read(element)
+
+        for pc_element in element.iterchildren(
+          '{http://namespaces.plone.org/supermodel/schema}portal_types'
+          ):
+            attributeField = self.fieldAttributes.get('portal_types', None)
+            portal_types = self.readAttribute(pc_element, attributeField)
+            if portal_types:
+                portal_types = [s for s in portal_types]
+                # setting vocabulary also sets source
+                field.vocabulary = ObjPathSourceBinder(portal_type=portal_types)
+            break
+
+        return field
+
     def write(self, field, name, type, elementName='field'):
 
-        if field.source is not None:
-            # The Choice field instantiates with both vocabulary and source
-            # set to the same value. We want to be able to use the Choice
-            # export handler, but avoid it writing out a vocabulary element.
-            source = field.source
-            field.vocabulary = None
+        # if field.source is not None:
+        #     # The Choice field instantiates with both vocabulary and source
+        #     # set to the same value. We want to be able to use the Choice
+        #     # export handler, but avoid it writing out a vocabulary element.
+        #     source = field.source
+        #     field.vocabulary = None
 
         element = super(RelationChoiceHandlerClass, self).write(field, name, type, elementName)
 
         # write source
-        if source is not None:
-            attributeField = self.fieldAttributes['source']
-            child = valueToElement(
-                attributeField,
-                "%s.%s" % (source.__module__, source.__name__),
-                name='source',
-                force=True
-                )
-            element.append(child)
+        # if source is not None:
+        #     attributeField = self.fieldAttributes['source']
+        #     child = valueToElement(
+        #         attributeField,
+        #         "%s.%s" % (source.__module__, source.__name__),
+        #         name='source',
+        #         force=True
+        #         )
+        #     element.append(child)
+
+        portal_types_field = self.fieldAttributes['portal_types']
+        source = field.source
+        if IContextSourceBinder.providedBy(source):
+            filter = getattr(source, 'selectable_filter', None)
+            if filter is not None:
+                criteria = filter.criteria
+                portal_types = criteria.get('portal_type')
+                if len(criteria.keys()) == 1 and \
+                   portal_types is not None:
+                    child = valueToElement(
+                        portal_types_field,
+                        portal_types,
+                        name='portal_types',
+                        force=True
+                        )
+                    element.append(child)
 
         return element
 
 
 RelationChoiceHandler = RelationChoiceHandlerClass(RelationChoice)
-RelationListHandler = BaseHandler(RelationList)
+# RelationListHandler = BaseHandler(RelationList)
--- plone/app/relationfield/schemaeditor.py
+++ plone/app/relationfield/schemaeditor.py
@@ -1,17 +1,43 @@
-from plone.app.relationfield import RelationChoice
-from plone.app.relationfield import RelationList
+
+# Support for editing RelationChoice and RelationList fields
+# via plone.schemaeditor.
+#
+# Goal, provide a schemaeditor interface that makes it easy
+# to do the most common thing: browse for relations by
+# portal type -- while having it still be possible to spec
+# custom sources by editing XML.
+#
+# Support for supermodel ex/im in exportimport.py
+
+from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.schemaeditor import SchemaEditorMessageFactory as _
 from plone.schemaeditor.fields import FieldFactory
 from plone.schemaeditor.fields import IFieldFactory
+from plone.schemaeditor.interfaces import IFieldEditFormSchema
+from z3c.relationfield.interfaces import IRelationChoice
+from z3c.relationfield.schema import RelationChoice
 from zope.app.intid.interfaces import IIntIds
+from zope.component import adapter
+from zope.component import adapts
 from zope.component import queryUtility
+from zope.interface import implementer
 from zope.interface import implements
-from zope.interface import directlyProvides
+from zope.interface import provider
 from zope.schema.interfaces import IContextSourceBinder
 
 import plone.formwidget.contenttree
+import zope.component
+import zope.interface
+import zope.schema
 
 
+# In order to appear in schemaeditor's list of addable/
+# editable fields, we need to supply a FieldFactory
+# as a utility.
+#
+# This requires a custom FieldFactory class so that we
+# can override the available method so that we don't
+# show the option when it doesn't make sense.
 class RelationFieldFactory(FieldFactory):
     implements(IFieldFactory)
 
@@ -19,11 +45,15 @@ class RelationFieldFactory(FieldFactory):
         return queryUtility(IIntIds) is not None
 
 
+# Our RelationFieldFactory will need to supply a
+# default 'source' attribute for the field it returns.
+# TODO: parameterize
+@provider(zope.schema.interfaces.IContextSourceBinder)
 def opsb(context):
     # provide a callable with a __name__attribute
     return plone.formwidget.contenttree.obj_path_src_binder(context)
 
-directlyProvides(opsb, IContextSourceBinder)
+# directlyProvides(opsb, zope.schema.interfaces.IContextSourceBinder)
 
 
 RelationChoiceFactory = RelationFieldFactory(
@@ -33,7 +63,78 @@ RelationChoiceFactory = RelationFieldFactory(
 )
 
 
-RelationListFactory = RelationFieldFactory(
-    RelationList,
-    _(u'label_relationlist_field', default=u'Relation List')
-)
+# Specify an editing interface and an adapter that will return it.
+
+class IEditableRelationChoice(zope.schema.interfaces.IField):
+    """XXX
+    """
+
+    source = zope.schema.DottedName(
+        title=_("Source providing values"),
+        description=_("The IContextSourceBinder "
+                      "object that provides values for this field."),
+        required=True,
+        default=None,
+        readonly=True,
+        )
+
+    portal_types = zope.schema.Set(
+        title=_(u"Target types to allow for relations"),
+        value_type=zope.schema.Choice(
+            vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes",
+            ),
+        )
+
+
+@implementer(IFieldEditFormSchema)
+@adapter(IRelationChoice)
+def getRelationChoiceFieldSchema(field):
+    return IEditableRelationChoice
+
+
+class EditableRelationChoiceField(object):
+    implements(IEditableRelationChoice)
+    adapts(RelationChoice)
+
+    def __init__(self, field):
+        self.__dict__['field'] = field
+
+    def __getattr__(self, name):
+        # if name == 'source':
+        #     source = getattr(self.field, name)
+        #     return "%s.%s" % (source.__module__, source.__name__)
+        if name == 'portal_types':
+            source = self.field.source
+            if IContextSourceBinder.providedBy(source):
+                filter = getattr(source, 'selectable_filter', None)
+                if filter is not None:
+                    criteria = filter.criteria
+                    portal_types = criteria.get('portal_type')
+                    if len(criteria.keys()) == 1 and \
+                       portal_types is not None:
+                        return portal_types
+            return []
+        return getattr(self.field, name)
+
+    def __setattr__(self, name, value):
+        if name == 'portal_types':
+            return setattr(
+                self.field,
+                'vocabulary',
+                ObjPathSourceBinder(portal_type=value)
+                )
+        return setattr(self.field, name, value)
+
+    def __delattr__(self, name):
+        # import pdb; pdb.set_trace()
+        return delattr(self.field, name)
+
+
+
+
+
+# RelationListFactory = RelationFieldFactory(
+#     RelationList,
+#     _(u'label_relationlist_field', default=u'Relation List')
+# )
+

db807270ab7b6ea89323ea596ec3fc8844f4ecfb
Steve McMahon <steve@dcn.org>
2014-01-03 09:24:52 -0800

    Restore marshaler tests; fix fail

--- plone/app/relationfield/tests.py
+++ plone/app/relationfield/tests.py
@@ -1,33 +1,15 @@
-# import unittest
-# from zope.testing import doctest
-# import zope.component.testing
-
-# class UnitTestLayer:
-
-#     @classmethod
-#     def testTearDown(cls):
-#         zope.component.testing.tearDown()
-
-# def test_suite():
-
-#     marshaler = doctest.DocFileSuite('marshaler.txt', optionflags=doctest.ELLIPSIS)
-#     marshaler.layer = UnitTestLayer
-
-#     return unittest.TestSuite((marshaler,))
-
-
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.testing import layered
-from plone.testing import z2
-
 
 import doctest
 import unittest
 
 
 class PloneAppRelationFieldLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
 
     def setUpZope(self, app, configurationContext):
         import plone.app.dexterity
@@ -39,9 +21,9 @@ class PloneAppRelationFieldLayer(PloneSandboxLayer):
         self.applyProfile(portal, 'plone.app.relationfield:default')
 
 
-MY_PRODUCT_FIXTURE = PloneAppRelationFieldLayer()
-MY_PRODUCT_INTEGRATION_TESTING = IntegrationTesting(
-    bases=(MY_PRODUCT_FIXTURE,),
+PARF_FIXTURE = PloneAppRelationFieldLayer()
+PARF_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(PARF_FIXTURE,),
     name="PloneAppRelationFieldLayer:Integration")
 
 optionflags = (doctest.ELLIPSIS |
@@ -52,7 +34,11 @@ def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
         layered(doctest.DocFileSuite('exportimport.txt', optionflags=optionflags),
-                MY_PRODUCT_INTEGRATION_TESTING)
+                PARF_INTEGRATION_TESTING)
+    ])
+    suite.addTests([
+        layered(doctest.DocFileSuite('marshaler.txt', optionflags=optionflags),
+                PARF_INTEGRATION_TESTING)
     ])
 
     return suite

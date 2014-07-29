c62b34e6d4c21e62d07a3896ad80855b12ed6d7d
Gil Forcada <gforcada@gnome.org>
2014-07-30 01:13:18 +0200

    .txt -> .rst

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ version = '1.2.3.dev0'
 setup(name='plone.app.relationfield',
       version=version,
       description="Plone support for z3c.relationfield",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.rst")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",

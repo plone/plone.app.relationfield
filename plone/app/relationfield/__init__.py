408fa93b1d8b8733bcf24d15d247987de379741c
vangheem <vangheem@gmail.com>
2015-01-26 12:50:10 -0600

    integrate plone.app.widgets

--- plone/app/relationfield/__init__.py
+++ plone/app/relationfield/__init__.py
@@ -5,20 +5,6 @@ PATCHES
 import pkg_resources
 
 try:
-    pkg_resources.get_distribution('plone.formwidget.contenttree')
-except pkg_resources.DistributionNotFound:
-    HAS_CONTENTTREE = False
-else:
-    HAS_CONTENTTREE = True
-
-try:
-    pkg_resources.get_distribution('plone.app.widgets')
-except pkg_resources.DistributionNotFound:
-    HAS_WIDGETS = False
-else:
-    HAS_WIDGETS = True
-
-try:
     pkg_resources.get_distribution('plone.app.contenttypes')
 except pkg_resources.DistributionNotFound:
     HAS_CONTENTTYPES = False
--- plone/app/relationfield/behavior.py
+++ plone/app/relationfield/behavior.py
@@ -1,52 +1,31 @@
 # -*- coding: utf-8 -*-
-from plone.app.relationfield import HAS_CONTENTTREE
-from plone.app.relationfield import HAS_WIDGETS
 from plone.autoform.interfaces import IFormFieldProvider
 from plone.supermodel.interfaces import FIELDSETS_KEY
 from plone.supermodel.model import Fieldset
 from z3c.relationfield.schema import RelationChoice
 from z3c.relationfield.schema import RelationList
-from zope.interface import Interface
 from zope.interface import alsoProvides
-
-if HAS_CONTENTTREE and not HAS_WIDGETS:
-    from plone.formwidget.contenttree import ObjPathSourceBinder
-
-try:
-    from plone.app.dexterity import MessageFactory as _
-except ImportError:
-    def MessageFactory(messageid, default=None):
-        return unicode(default)
-    _ = MessageFactory
+from plone.autoform import directives as form
+from plone.supermodel import model
+from plone.app.dexterity import MessageFactory as _
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
 
 
-class IRelatedItems(Interface):
+class IRelatedItems(model.Schema):
     """Behavior interface to make a Dexterity type support related items.
     """
 
-    if HAS_CONTENTTREE and not HAS_WIDGETS:
-
-        relatedItems = RelationList(
-            title=_(u'label_related_items', default=u'Related Items'),
-            default=[],
-            value_type=RelationChoice(
-                title=u"Related",
-                source=ObjPathSourceBinder()
-            ),
-            required=False
-        )
-
-    else:
-
-        relatedItems = RelationList(
-            title=_(u'label_related_items', default=u'Related Items'),
-            default=[],
-            value_type=RelationChoice(
-                title=u"Related",
-                vocabulary="plone.app.vocabularies.Catalog"
-            ),
-            required=False
-        )
+    relatedItems = RelationList(
+        title=_(u'label_related_items', default=u'Related Items'),
+        default=[],
+        value_type=RelationChoice(
+            title=u"Related",
+            vocabulary="plone.app.vocabularies.Catalog"
+        ),
+        required=False
+    )
+    form.widget('relatedItems', RelatedItemsFieldWidget,
+                vocabulary='plone.app.vocabularies.Catalog')
 
 fieldset = Fieldset('categorization',
                     label=_(u'Categorization'), fields=['relatedItems'])

e1aad5dd77e32fa79bc3e5c37361b778e51c1a66
Maurits van Rees <maurits@vanrees.org>
2016-02-09 22:07:06 +0100

    Added classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,55 +1,58 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.3.3.dev0'
 
-setup(name='plone.app.relationfield',
-      version=version,
-      description="Plone support for z3c.relationfield",
-      long_description=open("README.rst").read() + "\n" +
-                       open("CHANGES.rst").read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.relationfield',
+    version=version,
+    description="Plone support for z3c.relationfield",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='dexterity relations plone zc.relation',
-      author='Alec Mitchell',
-      author_email='apm13@columbia.edu',
-      url='https://pypi.python.org/pypi/plone.app.relationfield',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.interface',
-          'zope.component',
-          'zope.schema',
-          'zope.intid',
-          'five.intid',
-          'plone.app.intid',
-          'z3c.form',
-          'z3c.relationfield>=0.4.2',
-          'z3c.formwidget.query',
-          'plone.autoform',
-          'plone.supermodel',
-          'plone.app.vocabularies',
-          'plone.schemaeditor>=1.3.5.dev0',
-          'Products.CMFCore',
-          'plone.rfc822',
-          'plone.app.z3cform>=1.1.0.dev0'
-      ],
-      extras_require={
-          'test': [
-              'plone.app.testing',
-              'plone.app.dexterity',
-              'plone.app.robotframework',
-          ]
-      },
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    ],
+    keywords='dexterity relations plone zc.relation',
+    author='Alec Mitchell',
+    author_email='apm13@columbia.edu',
+    url='https://pypi.python.org/pypi/plone.app.relationfield',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.interface',
+        'zope.component',
+        'zope.schema',
+        'zope.intid',
+        'five.intid',
+        'plone.app.intid',
+        'z3c.form',
+        'z3c.relationfield>=0.4.2',
+        'z3c.formwidget.query',
+        'plone.autoform',
+        'plone.supermodel',
+        'plone.app.vocabularies',
+        'plone.schemaeditor>=1.3.5.dev0',
+        'Products.CMFCore',
+        'plone.rfc822',
+        'plone.app.z3cform>=1.1.0.dev0'
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'plone.app.dexterity',
+            'plone.app.robotframework',
+        ]
+    },
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    )

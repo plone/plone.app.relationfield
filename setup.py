a8bb76076fdeb005f4920466600aae42166d2530
Maurits van Rees <maurits@vanrees.org>
2016-03-08 18:37:35 +0100

    Renamed README.txt to README.rst.
    Removed README.md.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='plone.app.relationfield',
     version=version,
     description="Plone support for z3c.relationfield",
-    long_description=(open("README.txt").read() + "\n" +
+    long_description=(open("README.rst").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read()),
     # Get more strings from
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers

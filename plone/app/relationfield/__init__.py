94afc1e7296e4905c3babd1316448d4b621f0965
Gil Forcada Codinachs <gil.gnome@gmail.com>
2025-03-01 18:11:45 +0100

    chore: p.a.contenttypes is always there

--- plone/app/relationfield/__init__.py
+++ plone/app/relationfield/__init__.py
@@ -1,14 +1,4 @@
 from plone.app.relationfield.monkey import PATCHES
 
-import pkg_resources
-
 
 PATCHES
-
-
-try:
-    pkg_resources.get_distribution("plone.app.contenttypes")
-except pkg_resources.DistributionNotFound:
-    HAS_CONTENTTYPES = False
-else:
-    HAS_CONTENTTYPES = True
